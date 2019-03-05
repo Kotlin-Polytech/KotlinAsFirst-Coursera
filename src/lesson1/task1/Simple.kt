fcb5c0270a24c5d55eed79737df25f27bd8ddc51
Valter <valter70@i.ua>
2019-03-05 16:17:28 +0200

    Решение задачи - seconds

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -58,7 +58,7 @@ fun main(args: Array<String>) {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int = seconds + minutes * 60 + hours * 60 * 60
 
 /**
  * Тривиальная
