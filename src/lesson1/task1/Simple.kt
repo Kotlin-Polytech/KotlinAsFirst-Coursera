5e3728cc69615b8d625348d9002afced7b75c21a
mnewlive <vadimm@saltedge.com>
2019-07-16 11:17:45 +0300

    Calculated the time of the train on the way

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -112,7 +112,8 @@ fun thirdDigitCase2(number: Int): Int = (number / 100) % 10
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int =
+        hoursArrive * 60 + minutesArrive - (hoursDepart * 60 + minutesDepart)
 
 /**
  * Простая
