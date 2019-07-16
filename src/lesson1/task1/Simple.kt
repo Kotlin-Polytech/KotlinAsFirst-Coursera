030217d2f491323a783c4e5d1544df1e8b8786ba
mnewlive <vadimm@saltedge.com>
2019-07-16 11:29:48 +0300

    Added SSH key in github

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -122,7 +122,6 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-
 fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
 
 /**
