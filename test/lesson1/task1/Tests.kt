6d3f19f9c0921dc0388c417097cc5caed202c6e5
Aenfild <denis02fedorus@gmail.com>
2019-09-21 12:47:25 +0300

    update kotlin to 1.2.71 implement functions thirdNumber and angleInRadian

--- test/lesson1/task1/Tests.kt
+++ test/lesson1/task1/Tests.kt
@@ -84,7 +84,9 @@ class Tests {
         assertEquals(0, thirdDigit(1000))
         assertEquals(5, thirdDigit(4567))
         assertEquals(8, thirdDigit(2329858))
+        assertEquals(8, thirdDigit(2329858))
     }
+
 
     @Test
     @Tag("Easy")
